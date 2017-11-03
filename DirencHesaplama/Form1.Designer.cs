56bb472aa40514d0b87e5d8231ded13acdfc3120
Okan ALAN <okanalan97.14@gmail.com>
2017-11-03 23:24:09 +0300

    Add files via upload
    lastOne

---
+++ DirencHesaplama/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace DirencHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DirencHesaplama.Properties.Resources.direnç4;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 432);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(172, 25);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(350, 25);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(535, 25);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 4;
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Onlar Basamağı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Birler Basamağı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Çarpan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(532, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tolerans";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 82);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(294, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(238, 52);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(60, 213);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(98, 25);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(60, 253);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(98, 25);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(60, 292);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(98, 25);
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(60, 376);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(98, 25);
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(535, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "label6";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(297, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(334, 108);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(31, 20);
+            this.textBox2.TabIndex = 17;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(403, 108);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(374, 115);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 560);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DirencHesaplama/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DirencHesaplama
+{
+    enum Renkler
+    {
+        siyah , kahverengi , kırmızı , turuncu , sarı , yeşil , mavi , mor , gri , beyaz
+    }
+    enum Tolerans
+    {
+        kahverengi , kırmızı , yeşil , mavi , mor , gri
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string no1 = "", no2 = "", sayı;
+            double no3 = 0.0, carpım;
+            
+            no1 = comboBox1.SelectedIndex.ToString();
+            textBox1.Text = no1;
+            no2 = comboBox1.SelectedIndex.ToString();
+            textBox2.Text = no2;
+            no3 = comboBox3.SelectedIndex;
+            textBox3.Text = Math.Pow(10, no3).ToString();
+            // text1-2-3 readonly  özelliği açıldı sadece okunabiliyor.
+            sayı = no1 + no2;
+            carpım = (Convert.ToInt32(sayı))*Math.Pow(10,no3);
+
+            label5.Text = carpım.ToString();
+
+
+
+        }
+        void tolerans()
+        {
+            switch (comboBox4.Text)
+            {
+                case "kahverengi": label6.Text = "+ - %1"; break;
+                case "kırmızı": label6.Text = "+ - %2"; break;
+                case "yeşil": label6.Text = "+ - %5"; break;
+                case "mavi": label6.Text = "+ - %0.25"; break;
+                case "mor": label6.Text = "+ - %0.10"; break;
+                case "gri": label6.Text = "+ - %0.05"; break;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string b in Enum.GetNames(typeof(Renkler)))
+            {
+                comboBox1.Items.Add(b);
+                comboBox2.Items.Add(b);
+                comboBox3.Items.Add(b);
+            }
+            foreach( string a in Enum.GetNames(typeof(Tolerans)))
+                comboBox4.Items.Add(a);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "siyah": pictureBox2.BackColor = System.Drawing.Color.Black; break;
+                case "kahverengi": pictureBox2.BackColor = System.Drawing.Color.Brown; break;
+                case "kırmızı": pictureBox2.BackColor = System.Drawing.Color.Red; break;
+                case "turuncu": pictureBox2.BackColor = System.Drawing.Color.Orange; break;
+                case "sarı": pictureBox2.BackColor = System.Drawing.Color.Yellow; break;
+                case "yeşil": pictureBox2.BackColor = System.Drawing.Color.Green; break;
+                case "mavi": pictureBox2.BackColor = System.Drawing.Color.Blue; break;
+                case "mor": pictureBox2.BackColor = System.Drawing.Color.Purple; break;
+                case "gri": pictureBox2.BackColor = System.Drawing.Color.Gray; break;
+                case "beyaz": pictureBox2.BackColor = System.Drawing.Color.White; break;
+            }
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.Text)
+            {
+                case "siyah": pictureBox3.BackColor = System.Drawing.Color.Black; break;
+                case "kahverengi": pictureBox3.BackColor = System.Drawing.Color.Brown; break;
+                case "kırmızı": pictureBox3.BackColor = System.Drawing.Color.Red; break;
+                case "turuncu": pictureBox3.BackColor = System.Drawing.Color.Orange; break;
+                case "sarı": pictureBox3.BackColor = System.Drawing.Color.Yellow; break;
+                case "yeşil": pictureBox3.BackColor = System.Drawing.Color.Green; break;
+                case "mavi": pictureBox3.BackColor = System.Drawing.Color.Blue; break;
+                case "mor": pictureBox3.BackColor = System.Drawing.Color.Purple; break;
+                case "gri": pictureBox3.BackColor = System.Drawing.Color.Gray; break;
+                case "beyaz": pictureBox3.BackColor = System.Drawing.Color.White; break;
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox3.Text)
+            {
+                case "siyah": pictureBox4.BackColor = System.Drawing.Color.Black; break;
+                case "kahverengi": pictureBox4.BackColor = System.Drawing.Color.Brown; break;
+                case "kırmızı": pictureBox4.BackColor = System.Drawing.Color.Red; break;
+                case "turuncu": pictureBox4.BackColor = System.Drawing.Color.Orange; break;
+                case "sarı": pictureBox4.BackColor = System.Drawing.Color.Yellow; break;
+                case "yeşil": pictureBox4.BackColor = System.Drawing.Color.Green; break;
+                case "mavi": pictureBox4.BackColor = System.Drawing.Color.Blue; break;
+                case "mor": pictureBox4.BackColor = System.Drawing.Color.Purple; break;
+                case "gri": pictureBox4.BackColor = System.Drawing.Color.Gray; break;
+                case "beyaz": pictureBox4.BackColor = System.Drawing.Color.White; break;
+            }
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {         
+            switch (comboBox4.Text)
+            {
+                case "kahverengi": pictureBox5.BackColor = System.Drawing.Color.Brown; break;
+                case "kırmızı": pictureBox5.BackColor = System.Drawing.Color.Red; break;
+                case "yeşil": pictureBox5.BackColor = System.Drawing.Color.Green; break;
+                case "mavi": pictureBox5.BackColor = System.Drawing.Color.Blue; break;
+                case "mor": pictureBox5.BackColor = System.Drawing.Color.Purple; break;
+                case "gri": pictureBox5.BackColor = System.Drawing.Color.Gray; break;
+            }
+            tolerans();
+        }
+    }
+}
